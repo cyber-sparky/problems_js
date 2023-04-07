5a4e98ad50a522a6bacad8253f19a3526a6082ab
cyber-sparky <85377859+cyber-sparky@users.noreply.github.com>
2023-04-07 10:02:13 +0530

    Create superstar.js

---
+++ bharathwajProblems/superstar.js
@@ -0,0 +1,11 @@
+for (let i = 0; i <= 100; i++) {
+  console.log("***SuperStar***");
+}
+
+for (let i = 1; i <= 100; i++) {
+  console.log(i+"***SuperStar***");
+}
+
+for (let i = 1; i <= 100; i++) {
+  console.log(i++ +"superstar\n"+i +"rajini");
+}
